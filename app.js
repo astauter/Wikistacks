481b95534595cfd5f1337768eb1c7a2a3e82c594
Alex Stauter <ajstauter@gmail.com>
2017-06-16 11:59:47 -0500

    more progress

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const nunjucks = require('nunjucks');
 const path = require('path')
+const models = require('./models/index.js')
 
 
 
@@ -15,3 +16,11 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '/public')))
 app.use(bodyParser.urlencoded({ extended: true }))
 //app.use(bodyParser.json());
+
+models.db.sync({force: true})
+.then(function () {
+    app.listen(1337, function() {
+    console.log('listening on 1337')
+    })
+}).catch(console.err);
+
---
+++ models/index.js
@@ -0,0 +1,40 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('postgres://localhost:5432/wikistack')
+
+var Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    urlTitle: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    }
+});
+
+var User = db.define('user', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+
+
+module.exports = {
+  Page: Page,
+  User: User,
+  Sequelize: Sequelize,
+  db: db
+};
